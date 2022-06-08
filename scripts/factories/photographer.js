dbfc68f8f2a669a6411622f95e202c87592bf547
Charline Ramelet <charline.ramelet@gmail.com>
2022-06-08 16:57:20 +0200

    Ajout paragraphes Card dans index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,10 +7,22 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const nameCard = document.createElement( 'h2' );
+        // const cityCard = document.createElement( 'p' );
+        // const countryCard = document.createElement( 'p' );
+        const taglineCard = document.createElement( 'p' );
+        const locationCard = document.createElement( 'p' );
+        const priceCard = document.createElement( 'span' );
+        nameCard.textContent = name;
+        locationCard.textContent = country+" "+city;
+        // countryCard.textContent = country;
+        taglineCard.textContent = tagline;
+        priceCard.textContent = price+"€/jour";
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(nameCard);
+        article.appendChild(locationCard);
+        article.appendChild(taglineCard);
+        article.appendChild(priceCard);
         return (article);
     }
     return { name, picture, getUserCardDOM }
