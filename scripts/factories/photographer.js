284b5ee760d0fc02a232fdfa9ff61338f661df65
Charline Ramelet <charline.ramelet@gmail.com>
2022-06-09 10:44:19 +0200

    Lien actif index vers page photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,29 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
+  const photographerPage = `photographer.html`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const nameCard = document.createElement( 'h2' );
-        // const cityCard = document.createElement( 'p' );
-        // const countryCard = document.createElement( 'p' );
-        const taglineCard = document.createElement( 'p' );
-        const locationCard = document.createElement( 'p' );
-        const priceCard = document.createElement( 'span' );
-        nameCard.textContent = name;
-        locationCard.textContent = country+" "+city;
-        // countryCard.textContent = country;
-        taglineCard.textContent = tagline;
-        priceCard.textContent = price+"€/jour";
-        article.appendChild(img);
-        article.appendChild(nameCard);
-        article.appendChild(locationCard);
-        article.appendChild(taglineCard);
-        article.appendChild(priceCard);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const nameCard = document.createElement("h2");
+    const taglineCard = document.createElement("p");
+    const locationCard = document.createElement("p");
+    const priceCard = document.createElement("span");
+    const linkCard = document.createElement("a");
+    linkCard.setAttribute("href", photographerPage);
+
+    nameCard.textContent = name;
+    locationCard.textContent = country + ", " + city;
+    taglineCard.textContent = tagline;
+    priceCard.textContent = price + "€/jour";
+
+    article.appendChild(linkCard);
+    linkCard.appendChild(img);
+    linkCard.appendChild(nameCard);
+    article.appendChild(locationCard);
+    article.appendChild(taglineCard);
+    article.appendChild(priceCard);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
