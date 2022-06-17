7cd61d4d329752d86e21fc8bff8847413aa5fe83
Charline Ramelet <charline.ramelet@gmail.com>
2022-06-17 17:24:43 +0200

    Affichage grille gallerie

--- scripts/factories/media-factory.js
+++ scripts/factories/media-factory.js
@@ -1,28 +1,31 @@
 function mediaFactory(data, photographerName) {
   console.log(photographerName);
-    const { title, image, likes, date, price } = data;
+    const { title, image, likes, date } = data;
   
     const picture = `assets/images/${photographerName}/${image}`;
   
     function getMediaCardDOM() {
       const article = document.createElement("article");
+      const linkSmallImg = document.createElement("a");
+      linkSmallImg.setAttribute("href", "#");
+      linkSmallImg.classList.add("link-img-small");
       const imgCard = document.createElement("img");
       imgCard.setAttribute("src", picture);
+      imgCard.classList.add("img-small");
+
       const titleCard = document.createElement("h2");
       const likesCard = document.createElement("span");
       const dateCard = document.createElement("span");
-      const priceCard = document.createElement("span");
   
       titleCard.textContent = title;
       likesCard.textContent = likes;
       dateCard.textContent = date;
-      priceCard.textContent = price + "€/jour";
   
-      article.appendChild(imgCard);
+      article.appendChild(linkSmallImg);
+      linkSmallImg.appendChild(imgCard);
       article.appendChild(titleCard);
       article.appendChild(likesCard);
       article.appendChild(dateCard);
-      article.appendChild(priceCard);
       return article;
     }
     return { title, picture, getMediaCardDOM };
