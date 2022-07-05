323464a58af3bd60baac6a721974c30ea27ee58c
Charline Ramelet <charline.ramelet@gmail.com>
2022-07-05 15:17:45 +0200

    Edit commentaires

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,22 +1,10 @@
 const modal = document.querySelector("#contact_modal");
+const modalContent = document.querySelector(".modal");
 const closeForm = document.querySelector(".button-close-form");
 const openForm = document.querySelector(".open-form");
 const submitButton = document.querySelector(".submit-button");
 const backdrop = document.querySelector(".backdrop");
 const form = document.querySelector("#form");
-
-function displayModal(elem) {
-  elem.style.display = "flex";
-  backdrop.style.display = "block";
-  backdrop.style.opacity = "1";
-  modal.focus();
-}
-
-function closeModal(elem) {
-  elem.style.display = "none";
-  backdrop.style.display = "none";
-  backdrop.style.opacity = "0";
-}
 
 openForm.addEventListener("click", function () {
   displayModal(modal);
@@ -61,3 +49,16 @@ message.addEventListener("input", function () {
     console.log(this.value);
   }, 1000);
 });
+
+function displayModal(elem) {
+  elem.style.display = "flex";
+  backdrop.style.display = "block";
+  backdrop.style.opacity = "1";
+  modalContent.focus();
+}
+
+function closeModal(elem) {
+  elem.style.display = "none";
+  backdrop.style.display = "none";
+  backdrop.style.opacity = "0";
+}
